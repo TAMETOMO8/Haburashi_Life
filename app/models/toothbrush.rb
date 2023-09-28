9cdfeda10e08b97968220bc84162d0e1959822da
Ryusei <tearaito@gmail.com>
2023-09-28 22:46:19 +0900

    Add:Toothbrushモデルでバリデーションを定義

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -1,2 +1,11 @@
 class Toothbrush < ApplicationRecord
+  belongs_to :user
+
+  validates :item_name, presence: true
+  validates :item_url, presence: true
+  validates :item_image_urls, presence: true
+  validates :user_id, presence: true
+
+
+  
 end
