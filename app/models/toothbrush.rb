e822f66f56ae9764104ae0d137915ddc713dcc34
Ryusei <tearaito@gmail.com>
2023-10-06 19:25:42 +0900

    Add:楽天のItemCodeに関するバリデーションを追加

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -1,6 +1,7 @@
 class Toothbrush < ApplicationRecord
   belongs_to :user
 
+  validates :item_code, presence: true
   validates :item_name, presence: true
   validates :item_url, presence: true
   validates :item_image_urls, presence: true
