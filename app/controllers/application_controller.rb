849d7151391182bd0ff4add1dea29e69ed44e6e7
Ryusei <tearaito@gmail.com>
2023-10-20 17:22:51 +0900

    Add:ユーザーがログインしている状態か判定する機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
+
+  private
 
   def current_user
     return unless session[:user_id]
 
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
