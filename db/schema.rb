00ae7bb004f544e4e4fa9aefe0326a7cd3f3ccf1
Ryusei <tearaito@gmail.com>
2023-10-27 20:02:07 +0900

    Fix:カラム名を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_145409) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_091630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_145409) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state", default: 0, null: false
+    t.integer "brush_material", default: 0, null: false
+    t.time "use_end_at"
     t.index ["user_id"], name: "index_toothbrushes_on_user_id"
   end
 
@@ -29,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_16_145409) do
     t.string "line_user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
+    t.string "name", default: "ゲスト", null: false
     t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 
