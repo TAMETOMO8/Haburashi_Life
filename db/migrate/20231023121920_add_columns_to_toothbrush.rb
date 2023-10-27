53860a18061099df250422d341760da5d89c5633
Ryusei <tearaito@gmail.com>
2023-10-27 19:44:46 +0900

    Add:今後使用するカラムを追加

--- db/migrate/20231023121920_add_columns_to_toothbrush.rb
+++ db/migrate/20231023121920_add_columns_to_toothbrush.rb
@@ -1,6 +1,7 @@
 class AddColumnsToToothbrush < ActiveRecord::Migration[7.0]
   def change
     add_column :toothbrushes, :status, :integer, null: false, default: 0
-    add_column :toothbrushes, :end_time, :datetime
+    add_column :toothbrushes, :brush_material, :integer, null: false, default: 0
+    add_column :toothbrushes, :use_end_at, :time
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_091630) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_145409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_091630) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "end_time"
     t.index ["user_id"], name: "index_toothbrushes_on_user_id"
   end
 
@@ -31,8 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_091630) do
     t.string "line_user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0, null: false
-    t.string "name", default: "ゲスト", null: false
     t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 
