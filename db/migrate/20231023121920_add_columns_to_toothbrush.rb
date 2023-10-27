6cadd20f093847190c283f7a3622f8f4da772237
Ryusei <tearaito@gmail.com>
2023-10-27 23:06:25 +0900

    Fix:一部修正

--- db/migrate/20231023121920_add_columns_to_toothbrush.rb
+++ db/migrate/20231023121920_add_columns_to_toothbrush.rb
@@ -2,6 +2,7 @@ class AddColumnsToToothbrush < ActiveRecord::Migration[7.0]
   def change
     add_column :toothbrushes, :state, :integer, null: false, default: 0
     add_column :toothbrushes, :brush_material, :integer, null: false, default: 0
+    add_column :toothbrushes, :hardness, :integer, null: false, default: 0
     add_column :toothbrushes, :use_end_at, :time
   end
 end
