47d7c17f9cc90bb50bc1e65ef9f708760e4b7d71
Ryusei <tearaito@gmail.com>
2023-10-28 15:49:23 +0900

    Fix:歯ブラシ登録の途中で別ページへ遷移した際に矛盾が発生しないように使用前に該当するenumを追加

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -7,7 +7,7 @@ class Toothbrush < ApplicationRecord
   validates :item_image_urls, presence: true
   validates :user_id, presence: true
 
-  enum state: { using: 0, end_used: 1, cleaning: 2, recycling: 3 }
+  enum state: { not_started: 0, using: 1, end_used: 2, cleaning: 3, recycling: 4 }
   enum brush_material: { nylon_hair: 0, animal_hair: 1, electric: 2, other: 3}
   enum hardness: { normal: 0, soft: 1, hard: 2 }
 end
