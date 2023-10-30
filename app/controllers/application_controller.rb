f025f5df6bea7fd1740507ffaf14ebb291c23946
Ryusei <tearaito@gmail.com>
2023-10-30 21:19:39 +0900

    Add:ログインしていない時にトップページへ遷移させるメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_login
+    return if logged_in?
+
+    flash[:notice] = 'ログインしてください'
+    redirect_to root_path
+  end
 end
