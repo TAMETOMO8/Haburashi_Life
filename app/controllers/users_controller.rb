41417662dc96f52dcf6397a1da129e409c61a5b0
Ryusei <tearaito@gmail.com>
2023-10-30 21:31:05 +0900

    Fix:rubocopによる指摘箇所を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to toothbrushes_path, success: "ユーザー名を更新しました"
+      redirect_to toothbrushes_path, success: 'ユーザー名を更新しました'
     else
-      flash.now[:danger] = "更新できませんでした"
+      flash.now[:danger] = '更新できませんでした'
       render :edit
     end
   end
