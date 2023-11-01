b2622a9b6466b00b656205ada8604cec6acdb3e1
Ryusei <tearaito@gmail.com>
2023-11-02 01:17:02 +0900

    Fix:一部修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: 'user_id', expire_after: 7.week
+Rails.application.config.session_store :cookie_store, key: 'user_id', expire_after: 5.days
