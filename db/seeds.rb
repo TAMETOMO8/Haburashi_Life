f1063e1bde8806ba25008ac60e36c4c7efd64095
Ryusei <tearaito@gmail.com>
2023-11-07 22:14:41 +0900

    Add:管理者用データとしてseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  name: 'シュルク',
+  line_user_id: ENV['ADMIN_LINE_ID'],
+  role: 1
+)
