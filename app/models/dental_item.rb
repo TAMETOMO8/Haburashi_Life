72eb7ab384e19df08125d53cd64746d826e157fa
Ryusei <tearaito@gmail.com>
2023-11-09 22:42:12 +0900

    Add:デンタルグッズの登録数を制限するバリデーションを追加

--- app/models/dental_item.rb
+++ app/models/dental_item.rb
@@ -4,4 +4,14 @@ class DentalItem < ApplicationRecord
 
   validates :toothbrush_id, presence: true
   validates :user_id, presence: true
+
+  validate :validate_dental_items_count, on: :create
+
+  private
+
+  def validate_dental_items_count
+    if user&.dental_items&.where(toothbrush_id:)&.count.to_i >= 5
+      errors.add(:base, '5つまでしか登録できません')
+    end
+  end
 end
