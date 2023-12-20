f0af55d6ebebebf4fa8fde84133f7d027ce6f274
Ryusei <tearaito@gmail.com>
2023-12-20 21:11:43 +0900

    Add:LINE送信用のカラムを追加

--- db/migrate/20231220073218_add_comment_to_toothbrush.rb
+++ db/migrate/20231220073218_add_comment_to_toothbrush.rb
@@ -1,5 +1,6 @@
 class AddCommentToToothbrush < ActiveRecord::Migration[7.0]
   def change
     add_column :toothbrushes, :comment, :string
+    add_column :toothbrushes, :comment, :string, default: 'false'
   end
 end
