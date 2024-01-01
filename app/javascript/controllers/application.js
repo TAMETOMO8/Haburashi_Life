008ab0164b67e459f79101e1afa3745a62dd2493
Ryusei <tearaito@gmail.com>
2024-01-01 10:47:16 +0900

    Delete:rails-ujsに関する記述を削除

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import { Autocomplete } from 'stimulus-autocomplete'
 
 const application = Application.start()
+application.register('autocomplete', Autocomplete)
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-import Rails from "@rails/ujs"
-Rails.start();
