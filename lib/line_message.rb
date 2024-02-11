a06c4a8a8e86f298cc426f246e52f667cd8fcde4
Ryusei <tearaito@gmail.com>
2024-02-11 22:24:15 +0900

    Add:Flex Messageを使用する形に変更 #95

--- lib/line_message.rb
+++ lib/line_message.rb
@@ -1,14 +1,79 @@
 module LineMessage
   module_function
 
-  def send_message_to_user(line_user_id, message_text)
+  def send_message_to_user(line_user_id, alt_text, header_text, hero_image, item_name,
+    contents_text, label_text, link_uri)
     require 'line/bot'
     client = Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     end
 
-    message = { type: 'text', text: message_text }
+    message = {
+      "type": "flex",
+      "altText": alt_text,
+      "contents": {
+        "type": "bubble",
+        "header": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "weight": "bold",
+              "text": header_text
+            }
+          ],
+          "alignItems": "center"
+        },
+        "hero": {
+          "type": "image",
+          "url": hero_image,
+          "size": "full"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": item_name,
+              "wrap": true,
+              "size": "sm",
+              "weight": "bold"
+            },
+            {
+              "type": "text",
+              "text": contents_text,
+              "wrap": true,
+              "margin": "md"
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": label_text,
+                "uri": link_uri
+              }
+            }
+          ]
+        },
+        "styles": {
+          "hero": {
+            "separator": true
+          },
+          "footer": {
+            "separator": true
+          }
+        }
+      }
+    }
 
     response = client.push_message(line_user_id, message)
     p response
