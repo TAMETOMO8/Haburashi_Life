b80c384007d67a511abccd87728779ccfbfba0fc
Ryusei <tearaito@gmail.com>
2024-02-19 22:10:00 +0900

    Add:OGPの設定に必要なものを追加 #98

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '歯ブラシLife ~Make up environment!~',
+      title: '歯ブラシの情報共有アプリ',
+      reverse: true,
+      charset: 'utf-8',
+      description: '使っている歯ブラシの情報を共有し合うことで、新しい歯ブラシを探せます',
+      keywords: '歯ブラシ,デンタルケア,歯ブラシのリサイクル',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('logo.png') },
+        { href: image_url('ogp_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp_image.jpg'),
+        local: 'ja-JP',
+      },
+      twitter: {
+        card: 'summary_large_image' # summaryかsummary_large_imageでサイズ選択
+      }
+    }
+  end
 end
