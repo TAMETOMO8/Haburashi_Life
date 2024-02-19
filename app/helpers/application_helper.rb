d06417f3f8e84744fb0f3352fcdd11d8259b5ef5
Ryusei <tearaito@gmail.com>
2024-02-19 22:21:38 +0900

    Fix:拡張子を修正 #98

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,7 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp_image.jpg'),
+        image: image_url('ogp_image.png'),
         local: 'ja-JP',
       },
       twitter: {
