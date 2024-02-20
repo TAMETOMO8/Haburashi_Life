4294b3ea9aea0a0e6146d66d456914c2c56f0671
Ryusei <tearaito@gmail.com>
2024-02-20 09:10:45 +0900

    Add:アダプターをsidekiqにするコードを追加 #97

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,6 @@ module HaburashiLife
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.active_job.queue_adapter = :sidekiq
   end
 end
