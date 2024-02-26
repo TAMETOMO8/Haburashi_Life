828b760bc4b29572eff172f0b065f9751281c94c
Ryusei <tearaito@gmail.com>
2024-02-26 23:49:19 +0900

    TEST:試験的にジョブメソッドを作成 #97

--- app/jobs/exchange_brush.rb
+++ app/jobs/exchange_brush.rb
@@ -1,8 +1,21 @@
-class ExchangeBlushJob < ApplicationJob
+class ExchangeBrushJob < ApplicationJob
   queue_as :default
 
   def perform(toothbrush)
     # Do something later
-    toothbrush.exchange_brush
+    toothbrush = Toothbrush.find(toothbrush)
+    line_user_id = toothbrush.user.line_user_id
+
+    alt_text = "交換日が来た歯ブラシがあります"
+    header_text = "歯ブラシの交換のお知らせです！"
+    hero_image = toothbrush.item_image_urls.to_s
+    item_name = toothbrush.item_name.to_s
+    contents_text = "使い終わったあとはどうするかを決めましょう！"
+    label_text = "使い方を決める"
+    link_uri = "https://www.haburashi-life.com/toothbrushes/#{toothbrush_id}"
+
+    LineMessage.send_message_to_user(line_user_id, alt_text, header_text, hero_image, item_name,
+                                     contents_text, label_text, link_uri)
+    toothbrush.update!(state: :end_used)
   end
 end
