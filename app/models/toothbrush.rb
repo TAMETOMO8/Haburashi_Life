40bb57a6c4a1ab860a1d73b1702fe5c85664dd4d
Ryusei <tearaito@gmail.com>
2024-02-27 22:13:30 +0900

    Fix:本番環境に影響が出ないように追加した処理を一時削除

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -16,43 +16,4 @@ class Toothbrush < ApplicationRecord
   scope :end_toothbrushed, -> { using.where('end_use_at <= ?', Time.current) }
   scope :used, -> { where.not(state: :not_started) }
   scope :using_threedays, -> { used.where('created_at <= ?', Time.current - 3.days) }
-
-  def comment_notice_and_update # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
-    line_user_id = toothbrush.user.line_user_id
-
-    alt_text = "歯ブラシの使い心地をコメントしてみましょう！"
-    header_text = "この歯ブラシを使い始めて3日が経ちました!"
-    hero_image = toothbrush.item_image_urls.to_s
-    item_name = toothbrush.item_name.to_s
-    contents_text = "使ってみた感想をコメントしてみましょう！"
-    label_text = "コメントを書く"
-    link_uri = "https://www.haburashi-life.com/toothbrushes/#{toothbrush.id}/edit"
-
-    return unless toothbrush.comment_notice == 'false' # rubocop:disable Style/Next,Lint/RedundantCopDisableDirective
-
-    LineMessage.send_message_to_user(line_user_id, alt_text, header_text, hero_image, item_name,
-                                     contents_text, label_text, link_uri)
-    toothbrush.update!(comment_notice: 'true')
-  end
-
-  def notice_not_started # rubocop:disable Metrics/MethodLength,Lint/RedundantCopDisableDirective
-    line_user_id = toothbrush.user.line_user_id
-
-    alt_text = "使用開始前の歯ブラシがあります"
-    header_text = "この歯ブラシはまだ使用開始前です。"
-    hero_image = toothbrush.item_image_urls.to_s
-    item_name = toothbrush.item_name.to_s
-    contents_text = "歯ブラシを使用する場合は、使用開始ボタンをクリックしましょう！"
-    label_text = "歯ブラシを使い始める"
-    link_uri = "https://www.haburashi-life.com/toothbrushes/#{toothbrush.id}/edit"
-
-    LineMessage.send_message_to_user(line_user_id, alt_text, header_text, hero_image, item_name,
-                                     contents_text, label_text, link_uri)
-  end
-
-  def push_line_message
-    ExchangeBrushJob.perform_later(self)
-    CommentNoticeJob.perform_later(self)
-    NoticeNotStartedJob.perform_later(self)
-  end
 end
