4f29aa2341d57e03059ff3043a508152e630d22b
Ryusei <tearaito@gmail.com>
2024-03-04 13:44:41 +0900

    Fix:本番環境でエラーが出たので削除 #97

--- app/jobs/comment_notice_job.rb
+++
@@ -1,7 +0,0 @@
-class CommentNoticeJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    # Do something later
-  end
-end
--- app/jobs/notice_not_started_job.rb
+++
@@ -1,7 +0,0 @@
-class NoticeNotStartedJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    # Do something later
-  end
-end
