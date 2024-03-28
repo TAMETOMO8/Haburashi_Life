17ee31eaa165d8f506c29f8c4dfb964defb1a5ee
Ryusei <tearaito@gmail.com>
2024-03-28 11:16:08 +0900

    Add:過去の時間選択時の処理を追加したほか、テキストや感嘆符の有無を修正 #107

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -34,14 +34,14 @@ class ToothbrushesController < ApplicationController
 
   def create
     @toothbrush = current_user.toothbrushes.new(rakuten_params)
-    @toothbrush.save!
+    @toothbrush.save
     redirect_to edit_toothbrush_path(@toothbrush), success: '歯ブラシが選択されました！続けてブラシの素材、やわらかさ、使い終わる日を決めましょう！'
   end
 
   def update
     check_blank
-    @toothbrush.update!(toothbrush_params)
-    start_used
+    @toothbrush.update(toothbrush_params)
+    change_state
     redirect_to toothbrush_path(@toothbrush), success: '内容が登録されました！'
   end
 
@@ -53,12 +53,12 @@ class ToothbrushesController < ApplicationController
 
   def update_state
     new_state = params[:new_state]
-    if @toothbrush.end_used? && new_state.in?(%w[cleaning recycling]) || @toothbrush.cleaning? && new_state.in?(%w[recycling]) # rubocop:disable Layout/LineLength
-      @toothbrush.update!(state: new_state)
-      redirect_to environment_path, success: 'あなたのおかげで環境が整いました!ご協力ありがとうございます!'
+    if @toothbrush.end_used? && new_state.in?(%w[cleaning recycling]) || @toothbrush.cleaning? && new_state.in?(%w[recycling])
+      @toothbrush.update(state: new_state)
+      redirect_to environment_path, success: 'また少し、環境が整いました!ご協力ありがとうございます!'
     elsif new_state.in?(%w[end_used])
       @toothbrush.end_use_at = Time.current
-      @toothbrush.update!(state: new_state)
+      @toothbrush.update(state: new_state)
       push_end_used_message
       redirect_to toothbrush_path(@toothbrush), success: '歯ブラシを使い終わりました!'
     end
@@ -83,10 +83,12 @@ class ToothbrushesController < ApplicationController
     params.require(:toothbrush).permit(:end_use_at, :brush_material, :hardness, :state, :comment)
   end
 
-  def start_used
-    if @toothbrush.not_started? # rubocop:disable Style/GuardClause
+  def change_state
+    if @toothbrush.end_use_at < Time.current && @toothbrush.using? || @toothbrush.end_use_at < Time.current && @toothbrush.not_started?
+      @toothbrush.update(state: 2)
+    elsif @toothbrush.not_started?
       @toothbrush.update(state: 1)
-      push_register_message
+      push_start_message
     end
   end
 
@@ -94,10 +96,10 @@ class ToothbrushesController < ApplicationController
     params[:toothbrush][:comment] = '' if params[:toothbrush][:comment].blank?
   end
 
-  def push_register_message
+  def push_start_message
     line_user_id = current_user.line_user_id
-    alt_text = "新しい歯ブラシが登録されました！"
-    header_text = "新しい歯ブラシの登録です!"
+    alt_text = "新しい歯ブラシの使用を開始しました！"
+    header_text = "新しい歯ブラシの使用開始です!"
     hero_image = @toothbrush.item_image_urls.to_s
     item_name = @toothbrush.item_name.to_s
     contents_text = "歯ブラシの使用終了日を決めると、終了日が来た際にお知らせいたします!"
