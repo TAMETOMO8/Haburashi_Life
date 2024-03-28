418403e2509778cbfb6bc2a1b60cc299109d82d0
Ryusei <tearaito@gmail.com>
2024-03-28 11:13:59 +0900

    Add:過去の時間を選択した時にボタンが切り替わる処理を追加 #107

--- app/javascript/edit_form.js
+++ app/javascript/edit_form.js
@@ -15,3 +15,28 @@ document.addEventListener('turbo:load', () => {
     });
   }
 });
+
+
+document.addEventListener('turbo:load', () => {
+  const editBtn = document.querySelector('.edit-button');
+  const beforeBtn = document.querySelector('.time-before-button');
+  const timeSelect = document.querySelector('.time-select');
+  const timeNow = new Date();
+  timeNow.setHours(0, 0, 0, 0);
+
+  if (editBtn) {
+    timeSelect.addEventListener('change', () => {
+      const [year, month, day] = timeSelect.value.split('-');
+      const selectDay = new Date(year, month - 1, day);
+      selectDay.setHours(0, 0, 0, 0);
+
+      if (timeNow > selectDay) {
+        beforeBtn.style.display = 'block';
+        editBtn.style.display = 'none';
+      } else {
+        beforeBtn.style.display = 'none';
+        editBtn.style.display = 'block';
+      }
+    });
+  }
+});
