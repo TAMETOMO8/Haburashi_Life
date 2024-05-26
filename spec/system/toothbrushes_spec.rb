0a7243ae5b528178088e5e6f510d0d514fe1b500
Ryusei <tearaito@gmail.com>
2024-05-26 18:48:04 +0900

    Add:簡単なテストを作成

--- spec/system/toothbrushes_spec.rb
+++ spec/system/toothbrushes_spec.rb
@@ -1,39 +1,21 @@
+require 'rails_helper'
+
 RSpec.describe 'Toothbrushes', type: :system do
 
   describe 'ログイン前' do
-    describe 'ページ遷移確認' do
+    describe '歯ブラシのページ' do
       context '歯ブラシの検索ページにアクセス' do
         it '検索ページへのアクセスが失敗する' do
-          visit new_task_path
-          expect(page).to have_content('Login required')
-          expect(current_path).to eq login_path
+          visit toothbrush_search_path
+          expect(page).to have_content('ログインしてください')
+          expect(current_path).to eq root_path
         end
       end
 
-      context 'タスクの編集ページにアクセス' do
-        it '編集ページへのアクセスが失敗する' do
-          visit edit_task_path(task)
-          expect(page).to have_content('Login required')
-          expect(current_path).to eq login_path
-        end
-      end
-
-      context 'タスクの詳細ページにアクセス' do
-        it 'タスクの詳細情報が表示される' do
-          visit task_path(task)
-          expect(page).to have_content task.title
-          expect(current_path).to eq task_path(task)
-        end
-      end
-
-      context 'タスクの一覧ページにアクセス' do
-        it 'すべてのユーザーのタスク情報が表示される' do
-          task_list = create_list(:task, 3)
-          visit tasks_path
-          expect(page).to have_content task_list[0].title
-          expect(page).to have_content task_list[1].title
-          expect(page).to have_content task_list[2].title
-          expect(current_path).to eq tasks_path
+      context '歯ブラシの一覧ページにアクセス' do
+        it '歯ブラシが登録されていない一覧ページが表示される' do
+          visit toothbrushes_path
+          expect(page).to have_content('使用されている歯ブラシはありません')
         end
       end
     end
