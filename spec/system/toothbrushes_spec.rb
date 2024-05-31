73530e98d0a6323331c3b758f5dcd4d5b89b41e8
Ryusei <tearaito@gmail.com>
2024-05-31 23:58:42 +0900

    Add:テストを追加

--- spec/system/toothbrushes_spec.rb
+++ spec/system/toothbrushes_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'Toothbrushes', type: :system do
           expect(current_path).to eq root_path
         end
       end
+
       describe '歯ブラシのページ' do
         context '歯ブラシの一覧ページにアクセス' do
           it '歯ブラシが登録されていない一覧ページが表示される' do
@@ -34,6 +35,7 @@ RSpec.describe 'Toothbrushes', type: :system do
             expect(page).to have_content('最初へ')
           end
         end
+
         context '歯ブラシの詳細ページにアクセス' do
           it '歯ブラシの詳細ページが表示される' do
             create(:toothbrush)
@@ -43,6 +45,7 @@ RSpec.describe 'Toothbrushes', type: :system do
             expect(page).to have_content('使いやすい歯ブラシです!')
           end
         end
+
         context 'ユーザーの詳細ページにアクセス' do
           it 'ユーザー詳細ページが表示される' do
             create(:toothbrush)
@@ -55,6 +58,7 @@ RSpec.describe 'Toothbrushes', type: :system do
         end
       end
     end
+
     describe '新規ログイン' do
       context 'ログインページにアクセス' do
         before do
@@ -76,6 +80,7 @@ RSpec.describe 'Toothbrushes', type: :system do
       create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return('user_session')
     end
+
     context '歯ブラシの検索ページにアクセス' do
       it '検索ページでテキスト入力し、検索に成功する' do
         visit root_path
@@ -100,6 +105,14 @@ RSpec.describe 'Toothbrushes', type: :system do
         click_link '新しい歯ブラシ'
         click_on '検索'
         expect(page).to have_content '検索ワードを入力してください'
+      end
+      it '検索ページで入力したテキストに該当する歯ブラシが見つからず、検索に失敗する' do
+        visit root_path
+        click_on '歯ブラシ'
+        click_link '新しい歯ブラシ'
+        fill_in 'text-form', with: 'パソコン'
+        click_on '検索'
+        expect(page).to have_content 'ごめんなさい！このキーワードを持つ歯ブラシは、楽天市場には無いみたいです・・・'
       end
     end
   end
